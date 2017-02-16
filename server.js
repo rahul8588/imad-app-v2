9eb7c6ed84f4f268b00cf74100138ab503a5cdbe
rahul8588 <rahulthegamester@gmail.com>
2017-02-16 23:13:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get('/article-one', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function(req, res){
-    res.send('article two will be shown');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function(req, res){
-    res.send('article three will be shown');
+    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 
